d3ec3daec129af0c5a7bdad454ee99db97120194
Samyuktha <102202003@smail.iitpkd.ac.in>
2024-03-15 17:05:35 +0530

    s4 core and sme

--- function.py
+++ function.py
@@ -46,57 +46,88 @@ branchnumber = {
 }
 
 def getBtechData(sem,branch):
-    data_list = []
-    dictarray = []
+    core_list = []
+    sme_list = []
+    hse_list = []
+    dictarray = {}
+    # courses = []
     bnum = branchnumber[branch]
     if sem in [6,8]:
         with open(f"csv files/{bnum}_UG_{branch}_TimeTable_Jan-May-2024.xlsx - s{sem}.csv", newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                data_list.append(row)
+                core_list.append(row)
     elif sem == 4:
         with open(f"csv files/{bnum}_UG_{branch}_TimeTable_Jan-May-2024.xlsx - s2,s4.csv", newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                data_list.append(row)
+                core_list.append(row)
+        with open("csv files/01_UG_CommonCourses_TimeTable_Jan-May-2024.xlsx - S2, S4 SME.csv", newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                sme_list.append(row)
+        
+        
     elif sem == 2:
         with open("csv files/01_UG_CommonCourses_TimeTable_Jan-May-2024.xlsx - S2.csv", newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                data_list.append(row)
-    reduced_data = []
-    for i in range(5,len(data_list)):
-        reduced_data.append(data_list[i])
-    courseCodes = []
-    courseNames = []   
-    classSlots = []
-    for r in reduced_data:
-        courseCodes.append(r[0])
-        courseNames.append(r[1])
-        classSlots.append(r[6])
-    classes = []
-    labs = []
-    for c in classSlots:
-        l = [s.strip() for s in c.split(",")]
-        classes.append(l[0])
-        if len(l)>1:
-            labs.append(l[1])
-        else:
-            labs.append(0)
-    for i in range(len(courseCodes)):
-        dictarray.append({courseCodes[i]: [courseNames[i],classes[i],labs[i]]})
-    courses = []
-    for dic in dictarray:
-        for key, value in dic.items():
-            if key == '':
+                core_list.append(row)
+    reduced_core = []
+    reduced_sme = []
+    for i in range(5,len(core_list)):
+        reduced_core.append(core_list[i])
+    for i in range(5,len(sme_list)):
+        reduced_sme.append(sme_list[i])
+    # print(sme_list)
+    # print(reduced_sme)
+    def getList(data):
+        courses = []
+        courseCodes = []
+        courseNames = []   
+        classSlots = []
+        for r in data:
+            courseCodes.append(r[0])
+            courseNames.append(r[1])
+            classSlots.append(r[6])
+        classes = []
+        labs = []
+        for c in classSlots:
+            l = [s.strip() for s in str(c).split(",")]
+            classes.append(l[0])
+            if len(l)>1:
+                labs.append(l[1])
+            else:
+                labs.append(0)
+        for i in range(len(courseCodes)):
+            if courseCodes[i] == '':
                 break
+            elif not courseCodes[i][0].isalpha():
+                break
+            courses.append([courseCodes[i],courseNames[i],classes[i],labs[i]])
+        return courses
+    
+    # print(courses)
+    dictarray["CORE"] = getList(reduced_core)
+    dictarray["SME"] = getList(reduced_sme)
+    # courses = []
+    # for dic in dictarray:
+    #     for key in dic:
+    #         if key == '':
+    #             break
             #if key != '':
-            if key[0].isalpha():
-                courses.append(dic)
+            # if str(key)[0].isalpha():
+            #     courses.append(dic)
             # if [k.isalpha() for k in key]:
             #     print(k)
             #     courses.append(dic)
-    return courses
     
-print(getBtechData(4,"CSE"))
+    return dictarray
+    
+d = getBtechData(4,"CSE")
+for key, value in d.items():
+    print(key,"-")
+    for v in value:
+        print(v)
+# print(d)
                 
