e8f4f05629fbbc42e76f18b84547c950c29298d3
Samyuktha <102202003@smail.iitpkd.ac.in>
2024-03-17 00:05:00 +0530

    btech done

--- function.py
+++ function.py
@@ -46,9 +46,12 @@ branchnumber = {
 }
 
 def getBtechData(sem,branch):
+    common_list = []
     core_list = []
     sme_list = []
     hse_list = []
+    pme_list = []
+    gce_list = []
     dictarray = {}
     # courses = []
     bnum = branchnumber[branch]
@@ -57,6 +60,15 @@ def getBtechData(sem,branch):
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
                 core_list.append(row)
+        with open(f"csv files/{bnum}_UG_{branch}_TimeTable_Jan-May-2024.xlsx - PME_ERC.csv", newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                pme_list.append(row)
+        with open(f"csv files/01_UG_CommonCourses_TimeTable_Jan-May-2024.xlsx - GCE.csv", newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                gce_list.append(row)
+       
     elif sem == 4:
         with open(f"csv files/{bnum}_UG_{branch}_TimeTable_Jan-May-2024.xlsx - s2,s4.csv", newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
@@ -70,33 +82,91 @@ def getBtechData(sem,branch):
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
                 hse_list.append(row)
+        with open(f"csv files/01_UG_CommonCourses_TimeTable_Jan-May-2024.xlsx - GCE.csv", newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                gce_list.append(row)
         
         
     elif sem == 2:
         with open("csv files/01_UG_CommonCourses_TimeTable_Jan-May-2024.xlsx - S2.csv", newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                core_list.append(row)
-    reduced_core = []
-    reduced_sme = []
-    reduced_hse = []
-    for i in range(5,len(core_list)):
-        reduced_core.append(core_list[i])
-    for i in range(5,len(sme_list)):
-        reduced_sme.append(sme_list[i])
-    for i in range(5,len(hse_list)):
-        reduced_hse.append(hse_list[i])
+                common_list.append(row)
+    
+    # reduced_core = []
+    # reduced_sme = []
+    # reduced_hse = []
+    # reduced_pme = []
+    # for i in range(5,len(core_list)):
+    #     reduced_core.append(core_list[i])
+    # for i in range(5,len(sme_list)):
+    #     reduced_sme.append(sme_list[i])
+    # for i in range(5,len(hse_list)):
+    #     reduced_hse.append(hse_list[i])
+    # for i in range(5, len(pme_list)):
+    #     reduced_pme.append(pme_list[i])
     # print(sme_list)
     # print(reduced_sme)
-    def getList(data):
+    
+    def getCommon(d):
+        data = []
+        for i in range(4,len(d)):
+            data.append(d[i])
         courses = []
         courseCodes = []
         courseNames = []   
         classSlots = []
-        for r in data:
-            courseCodes.append(r[0])
-            courseNames.append(r[1])
-            classSlots.append(r[6])
+
+        for i in range(len(data)):
+            r = data[i]
+            if r[0]:
+                courseCodes.append(r[0])
+                courseNames.append(r[1])
+                classSlots.append(r[4])
+        classes = []
+        labs = []
+        for c in classSlots:
+            l = [s.strip() for s in str(c).split(",")]
+            classes.append(l[0])
+            if len(l)>1:
+                labs.append(l[1])
+            else:
+                labs.append(0)
+        for i in range(len(courseCodes)):
+            if courseCodes[i] == '':
+                break
+            elif not courseCodes[i][0].isalpha():
+                break
+            courses.append([courseCodes[i],courseNames[i],classes[i],labs[i]])
+        return courses
+
+    def getList(d):
+        data = []
+        # for i in range(5,len(d)):
+        #     data.append(d[i])
+
+        courses = []
+        courseCodes = []
+        courseNames = []   
+        classSlots = []
+
+        if d is common_list:
+            for i in range(4,len(d)):
+                data.append(d[i])
+            for i in range(len(data)):
+                r = data[i]
+                if r[0]:
+                    courseCodes.append(r[0])
+                    courseNames.append(r[1])
+                    classSlots.append(r[4])
+        else:
+            for i in range(5,len(d)):
+                data.append(d[i])
+            for r in data:
+                courseCodes.append(r[0])
+                courseNames.append(r[1])
+                classSlots.append(r[6])
         classes = []
         labs = []
         for c in classSlots:
@@ -115,9 +185,12 @@ def getBtechData(sem,branch):
         return courses
     
     # print(courses)
-    dictarray["CORE"] = getList(reduced_core)
-    dictarray["SME"] = getList(reduced_sme)
-    dictarray["HSE"] = getList(reduced_hse)
+    dictarray["CORE"] = getList(core_list)
+    dictarray["SME"] = getList(sme_list)
+    dictarray["HSE"] = getList(hse_list)
+    dictarray["PME"] = getList(pme_list)
+    dictarray["GCE"] = getList(gce_list)
+    dictarray["Common"] = getList(common_list)
     # courses = []
     # for dic in dictarray:
     #     for key in dic:
@@ -132,10 +205,11 @@ def getBtechData(sem,branch):
     
     return dictarray
     
-d = getBtechData(4,"CSE")
+d = getBtechData(2,"CSE")
 for key, value in d.items():
-    print(key,"-")
-    for v in value:
-        print(v)
+    if value:
+        print(key,"-")
+        for v in value:
+            print(v)
 # print(d)
                 
